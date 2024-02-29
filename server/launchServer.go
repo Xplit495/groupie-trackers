9de6d2262e85f68c48a50606a92fd8607150534a
carquentin <quentin.carrola@ynov.com>
2024-02-29 14:04:55 +0100

    Update

--- server/launchServer.go
+++ server/launchServer.go
@@ -1,7 +1,34 @@
 package server
 
-import "fmt"
+import (
+	"GroupieTrackers/pkg/utils"
+	"fmt"
+	"net/http"
+	"path/filepath"
+)
 
 func LaunchServer() {
-	fmt.Println("PIPI")
+	utils.ClearTerminal()
+
+	webDir := filepath.Join("web")
+	fileServer := http.FileServer(http.Dir(webDir))
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		if request.URL.Path == "/" {
+			http.ServeFile(writer, request, filepath.Join(webDir, "html", "index.html"))
+		} else {
+			fileServer.ServeHTTP(writer, request)
+		}
+	})
+
+	err := utils.OpenBrowser("http://localhost:8080/")
+	if err != nil {
+		fmt.Println("Failed to open browser:", err)
+		return
+	}
+
+	err1 := http.ListenAndServe(":8080", nil)
+	if err1 != nil {
+		fmt.Println("Failed to start server:", err1)
+		return
+	}
 }
