9ae33d4114ad607f350b4bc513b7d74c06fa748f
carquentin <quentin.carrola@ynov.com>
2024-03-07 13:41:41 +0100

    Try to pull

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,12 @@
+document.addEventListener('click', function(event) {
+    var isClickInsideSearchBox = document.getElementById('search-box').contains(event.target);
+    var isClickInsideSuggestions = document.getElementById('suggestions').contains(event.target);
+
+    if (!isClickInsideSearchBox && !isClickInsideSuggestions) {
+        document.getElementById('suggestions').style.display = 'none';
+    }
+});
+
 function searchArtist() {
     var input = document.getElementById('search-input').value;
     if (input.length > 0) {
@@ -19,7 +28,13 @@ function showSuggestions(suggestions) {
     if (suggestions.length > 0) {
         suggestions.forEach(artist => {
             var suggestionElement = document.createElement('div');
-            suggestionElement.innerHTML = `<a href="/artists.html?id=${artist.id}">${artist.name}</a>`;
+            suggestionElement.classList.add('suggestion-item');
+            suggestionElement.innerHTML = artist.name;
+
+            suggestionElement.addEventListener('click', function() {
+                window.location.href = "/artists.html?id=" + artist.id;
+            });
+
             suggestionsContainer.appendChild(suggestionElement);
         });
         suggestionsContainer.style.display = 'block';
